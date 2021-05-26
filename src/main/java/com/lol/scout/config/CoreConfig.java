bee453e865e90f36d45293f7d311fb1f22a5d6d8
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-05-27 00:11:22 +0200

    Just removed import

--- src/main/java/com/lol/scout/config/CoreConfig.java
+++ src/main/java/com/lol/scout/config/CoreConfig.java
@@ -1,7 +1,6 @@
 package com.lol.scout.config;
 
 import com.google.gson.GsonBuilder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
